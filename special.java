7d00fb697213005f763ffe7d74d8ac51967fc88b
Pavithirapalanimurugan <41098780+Pavithirapalanimurugan@users.noreply.github.com>
2018-07-23 09:35:39 +0530

    Create special.java

---
+++ special.java
@@ -0,0 +1,29 @@
+package exp2;
+import java.util.Arrays;
+import java.util.Scanner;
+public class special {
+
+	public static void main(String[] args) {
+		int num,count=0,i;
+		Scanner in = new Scanner(System.in);
+		num = in.nextInt();
+		int []arr = new int[num];
+		int a[] = new int[num];
+		for( i = 0 ; i < num ; i++){
+			arr[i] = in.nextInt();
+		}
+		 Arrays.sort(arr);
+	    for( i = 0 ; i < num ; i++){
+			count =0;
+	    for(int j = i ; j < num ; j++){
+           			if(arr[i] == arr[j]){
+           			count++;	
+           			}
+           		}
+		if(count > 1){
+			System.out.println(arr[i]);
+		}
+		}
+	}
+
+}
