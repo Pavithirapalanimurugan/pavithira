66f17cd9c04017949b49c60b64fb7b4ef111751f
Pavithirapalanimurugan <41098780+Pavithirapalanimurugan@users.noreply.github.com>
2018-08-06 17:58:01 +0530

    Create dot.java

---
+++ dot.java
@@ -0,0 +1,10 @@
+package milestone2;
+import java.util.Scanner;
+public class dot {
+public static void main(String[] args) {
+	String s;
+	Scanner in = new Scanner(System.in);
+	s = in.next();
+	System.out.println(s+".");
+}
+}
